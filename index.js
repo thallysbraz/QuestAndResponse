38b93e1aef77483101cdf17cb54a8756c000d5ff
thallysbraz <thallysbraz3@gmail.com>
2019-12-26 11:44:17 -0300

    Card de pergunta

--- index.js
+++ index.js
@@ -10,25 +10,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 //rotas
-app.get("/:nome/:lang", (req, res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  var exibirMSG = true;
-  var produtos = [
-    { nome: "Doritos", preco: 4.5 },
-    { nome: "Guarana", preco: 7 },
-    { nome: "Ruffles", preco: 5 },
-    { nome: "Batata Palha", preco: 3.75 },
-    { nome: "Hamburguer", preco: 1 }
-  ];
-
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "First",
-    msg: exibirMSG,
-    produtos: produtos
-  });
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
 });
 
 app.listen(PORT, () => {

afac09cdc4311e2555f4c39833a9c3171895315d
thallysbraz <thallysbraz3@gmail.com>
2019-12-26 10:53:42 -0300

    Estrutura condicional com EJS

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ app.set("view engine", "ejs");
 app.get("/:nome/:lang", (req, res) => {
   var nome = req.params.nome;
   var lang = req.params.lang;
+  var exibirMSG = true;
   res.render("index", {
     nome: nome,
     lang: lang,
-    empresa: "First"
+    empresa: "First",
+    msg: exibirMSG
   });
 });
 
